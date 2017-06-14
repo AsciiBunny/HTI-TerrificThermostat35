b466a77b1e1dbb0544be3432506ab09d84f13a57
ThomasDerwig <Thomas_Derwig@outlook.com>
2017-06-14 14:57:18 +0200

    wat gewerkt aan de main page

--- app/src/main/java/com/example/s157888/terrificthermostat35/MainActivity.java
+++ app/src/main/java/com/example/s157888/terrificthermostat35/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.s157888.terrificthermostat35;
 
-import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+import android.support.design.widget.BottomNavigationView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    int vtemp = 21;
+    TextView cTemp;
+    TextView sTemp;
+    private BottomNavigationView mBottomNav;
 
     public static final String EXTRA_MESSAGE = "com.example.s157888.terrificthermostat35.MESSAGE";
 
@@ -14,13 +23,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+
+        cTemp = (TextView) findViewById(R.id.cTemp);
+        sTemp = (TextView) findViewById(R.id.sTemp);
+        Button plus = (Button) findViewById(R.id.plus);
+        Button minus = (Button) findViewById(R.id.minus);
+        Button preset = (Button) findViewById(R.id.preset);
+
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vtemp++;
+                cTemp.setText(vtemp + " \u2103");
+                sTemp.setText(vtemp + " \u2103");
+
+            }
+        });
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vtemp--;
+                cTemp.setText(vtemp + " \u2103");
+                sTemp.setText(vtemp + " \u2103");
+
+            }
+        });
+
+
+
     }
 }
