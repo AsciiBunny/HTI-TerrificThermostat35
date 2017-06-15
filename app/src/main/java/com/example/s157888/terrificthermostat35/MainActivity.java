02df47dc4aa2870f0ff1b9f54d8a10e2c4865bf3
ThomasDerwig <Thomas_Derwig@outlook.com>
2017-06-15 14:37:22 +0200

    navigatie balk toegevoegd(non-functional) en wat veranderd aan hoe de current temp gedisplayed wordt

--- app/src/main/java/com/example/s157888/terrificthermostat35/MainActivity.java
+++ app/src/main/java/com/example/s157888/terrificthermostat35/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.s157888.terrificthermostat35;
 
+import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,9 +14,11 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     int vtemp = 21;
+    int cTempHold;
     TextView cTemp;
     TextView sTemp;
     private BottomNavigationView mBottomNav;
+
 
     public static final String EXTRA_MESSAGE = "com.example.s157888.terrificthermostat35.MESSAGE";
 
@@ -29,14 +32,25 @@ public class MainActivity extends AppCompatActivity {
         sTemp = (TextView) findViewById(R.id.sTemp);
         Button plus = (Button) findViewById(R.id.plus);
         Button minus = (Button) findViewById(R.id.minus);
-        Button preset = (Button) findViewById(R.id.preset);
+
 
         plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 vtemp++;
+                cTempHold = 11;
                 cTemp.setText(vtemp + " \u2103");
                 sTemp.setText(vtemp + " \u2103");
+                new CountDownTimer(1000, 1000) {
+                    public void onTick(long milisUntilFinished){
+
+                    }
+                    public void onFinish() {
+                            cTemp.setText(cTempHold + " \u2103");
+                    }
+                }.start();
+
+
 
             }
         });
@@ -46,6 +60,14 @@ public class MainActivity extends AppCompatActivity {
                 vtemp--;
                 cTemp.setText(vtemp + " \u2103");
                 sTemp.setText(vtemp + " \u2103");
+                new CountDownTimer(1000, 1000) {
+                    public void onTick(long milisUntilFinished){
+
+                    }
+                    public void onFinish() {
+                        cTemp.setText(cTempHold + " \u2103");
+                    }
+                }.start();
 
             }
         });
